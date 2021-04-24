6d7df756b57ea5b3585ca73c09173a5d1bb97eab
Lestyn Brooks <wiiman98989@gmail.com>
2021-04-24 17:00:10 -0400

    Changed name to input bc that was a major problem

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const Mongoose = require('mongoose');
 require('dotenv').config();
+const port = process.env.PORT;
 const User = require('./models/user');
+const axios = require('axios');
+const StatusCodes = require('http-status-codes');
+const BodyParser = require('body-parser');
+
+app.use(BodyParser.json());
 
 const doActionThatMightFailValidation = async (request, response, action) => {
     try {
@@ -22,18 +28,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/users', async (request, response) => {
+    await doActionThatMightFailValidation(request, response, async () => {
+        await new User(request.body).save();
+        response.sendStatus(StatusCodes.CREATED);
+    });
+});
+
 io.on('connection', (socket) => {
     socket.on('database entry', msg => {
         console.log(msg);
-        app.post('/users', async (req, res) => {
-            // req.body = '{ "name": ' + msg + ' }';
-            await doActionThatMightFailValidation(req, res, async () => {
-                await new User(req.body).save();
-                res.sendStatus(201);
-            });
-        });
+        axios.post('http://localhost:8080/users', {
+            "name": msg
+        }).then(res => {
+            console.log(`statusCode: ${res.statusCode}`);
+            console.log(res);
+        }).catch(error => {
+            console.error(error);
+        })
     });
+    socket.on('query', msg => {
+        query(msg).then(result => socket.emit('query-result', result)).catch(err => console.error(err))
+    })
 });
+
+const query = async data => {
+    const queryResult = await User.aggregate([])
+        .search({
+            autocomplete: {
+                path: 'name',
+                query: data
+            }
+        });
+    return queryResult;
+}
 
 (async () => {
     await Mongoose.connect(process.env.URI, {
@@ -42,5 +70,7 @@ io.on('connection', (socket) => {
         useFindAndModify: false,
         useCreateIndex: true,
     });
-    http.listen(process.env.PORT);
+    http.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}/`);
+    });
 })();
